60a2c4b3c6f1b928c9f4eba613d58731e7520552
ery-s <jack.erin2003@gmail.com>
2026-02-13 22:58:17 +0200

    combined and simplified methods

--- projects/js/game.js
+++ projects/js/game.js
@@ -1,52 +1,22 @@
 //game logic 
-function selectDifficulty() {
-    //when a user clicks a button checks the selected difficulty and returns the difficulty
+function createGrid() {
+    //create game grid based on the selected diffulty
 }
+function randomiseCards(cards) {}
+function flipCard(card) {}
+function checkMatch(card1, card2) {}
+function checkWin(){}
+
 function startGame() {
     //game stars, timer starts and count moves, grid displayed
-}
-function pauseGame() {
-    //when clicked , the time and count moves pauses, provides restart,continue, back to portfolio and game home screen
-}
-function gameOver() {
-    //display leader boards and if win or loose and then play again
 }
 function restartGame() {
     //reset grid, time and score
 }
-function continueGame() {
-    //resume time and score close pause menue
-}
-function backToPortfolio() {
-    //back to protfolio home
-}
-function createGrid() {
-    //create game grid based on the selected diffulty
-}
-function flipCard(){
-    //on click on card flip card over
-}
-function displayGrid() {
-    //display the grid of cards
-}
-function countMoves() {
-    //count each time a card is flipped
-}
-function resetMoves() {
+
+function updateMoves() {
     //set moves at 0
 }
-function displayMoves() {
-    //show moves
-}
-function trackGameTime() {
-    //game timer
-}
-function resetTime(){
+function updateTime(){
     //set time back to 0
-}
-function pausetime(){
-    //pause timer 
-}
-function displayGameTime(){
-    //show the game time
 }
