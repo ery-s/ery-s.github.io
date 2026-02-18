2906e1891df899b854c324882f29f254b9c8a0f4
ery-s <jack.erin2003@gmail.com>
2026-02-18 21:58:27 +0200

    removed console debugging logs

--- projects/js/game.js
+++ projects/js/game.js
@@ -130,32 +130,27 @@ function flipCard(cardElement, card) {
     
 }
 function checkMatch(card1, card2) {
+    //check if the 2 most recently flipped cards match
     if (card1.card.value === card2.card.value) {
-        console.log("MATCH! Adding matched class");
-        console.log("Card1 classes before:", card1.cardElement.className);
-        console.log("Card2 classes before:", card2.cardElement.className);
-
+        //if they match set their isMatched property to true and clear the flippedCards array
+       
         card1.card.isMatched = true;
         card2.card.isMatched = true;
         card1.cardElement.classList.add('matched');
         card2.cardElement.classList.add('matched');
-
-        console.log("Card1 classes after:", card1.cardElement.className);
-        console.log("Card2 classes after:", card2.cardElement.className);
-
         flippedCards = [];
+       
         matchedCardPairs++;
         checkWin();
-
     } else {
-        console.log("NO MATCH - flipping back");
+        //if they don't match flip cards over after a  delay and clear the flippedCards array
         setTimeout(() => {
             card1.card.isFlipped = false;
             card2.card.isFlipped = false;
             card1.cardElement.classList.remove('flipped');
             card2.cardElement.classList.remove('flipped');
             flippedCards = [];
-        }, 1000);
+        }, 1000); //1 sec delay before flipping back
     }
 }
 function checkWin(){
